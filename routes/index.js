34e785951e7ca9f92e30431ea937d7e2821409b4
raphaelSeguin <raphaelseguin@gmail.com>
2018-10-25 11:01:50 +0200

    staging facture

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,23 @@ const router = express.Router();
 
 const insertFacture = require('../insertFacture.js');
 
+let facture = {};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
+  res.render('index', facture);
 });
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
-  insertFacture(req.body);
+  facture = req.body;
   res.render('facture', req.body)
 });
+
+router.get('/imprimer', function(req, res, next) {
+  insertFacture(facture);
+  res.render('merci', {});
+});
+
 
 module.exports = router;
